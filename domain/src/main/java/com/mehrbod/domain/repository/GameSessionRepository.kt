d65c65166b246c3774384f5c9a15600a1b1e58b2
mehrbod <mehrbod.kh@gmail.com>
2021-08-25 10:44:59 +0430

    Add game session repository

---
+++ domain/src/main/java/com/mehrbod/domain/repository/GameSessionRepository.kt
@@ -0,0 +1,29 @@
+package com.mehrbod.domain.repository
+
+import com.mehrbod.domain.model.question.Answer
+import com.mehrbod.domain.model.question.Question
+
+/**
+ * This repository is responsible for storing and providing the information needed for each game
+ * session such as answered questions and those answers.
+ *
+ * NOTE: This should be implemented in data layer.
+ */
+interface GameSessionRepository {
+
+    /**
+     * It stores the answered question.
+     */
+    fun addAnsweredQuestion(question: Question, answer: Answer)
+
+    /**
+     * It returns all the answered questions added before. Returns empty list if nothing has beed
+     * added.
+     */
+    fun getAllAnsweredQuestions(): List<Pair<Question, Answer>>
+
+    /**
+     * Clears all the already added answers and questions related to them.
+     */
+    fun clear()
+}
